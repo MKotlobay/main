c1c4ed7877b53cfd726803cafec82617a7d0d1b3
Your Name <you@example.com>
2024-05-17 12:04:02 +0300

    test

---
+++ studying-c-sharp-mark-kotlobay/static-demo/Level1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_mark_kotlobay.static_demo
+{
+    internal class Level1
+    {
+        public static void RunMe()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ studying-c-sharp-mark-kotlobay/static-demo/Level2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_mark_kotlobay.static_demo
+{
+    internal class Level2
+    {
+    }
+}
