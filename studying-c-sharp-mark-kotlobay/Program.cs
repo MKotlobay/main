a04c89e71e23d060bbaf6247099d4c9387d33c00
Your Name <you@example.com>
2024-05-24 13:10:51 +0300

    Basic template objects made, CarApp made based on objects template

--- studying-c-sharp-mark-kotlobay/Program.cs
+++ studying-c-sharp-mark-kotlobay/Program.cs
@@ -1,9 +1,14 @@
-﻿using studying_c_sharp_mark_kotlobay.static_demo;
+﻿using studying_c_sharp_mark_kotlobay.basic_objects;
+using studying_c_sharp_mark_kotlobay.static_demo;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Level1.RunMe();
+        //Level1.RunMe();
+
+        //TestBasicObject.Demo();
+
+        CarApp.CarGarageHome();
     }
 }
---
+++ studying-c-sharp-mark-kotlobay/basic-objects/ObjectTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studying_c_sharp_mark_kotlobay.basic_objects
+{
+    public class ObjectTemplate
+    {
+        public int intProperty = 10;
+
+        public string stringProperty = "Some string";
+
+        public bool boolProperty = false;
+
+        public void ToggleBoolProperty()
+        {
+            boolProperty = !boolProperty;
+        }
+
+    }
+
+    public class TestBasicObject
+    {
+        public static void Demo()
+        {
+            ObjectTemplate template1 = new ObjectTemplate();
+
+            Console.WriteLine("Template1 = intProperty = {0}, stringProperty = {1}, boolProperty = {2}", template1.intProperty, template1.stringProperty, template1.boolProperty);
+            template1.intProperty = 100;
+            template1.stringProperty = "hello";
+            template1.boolProperty = true;
+
+            Console.WriteLine("Template1 = intProperty = {0}, stringProperty = {1}, boolProperty = {2}", template1.intProperty, template1.stringProperty, template1.boolProperty);
+            ObjectTemplate template2 = new ObjectTemplate();
+
+            Console.WriteLine("Template2 = intProperty = {0}, stringProperty = {1}, boolProperty = {2}", template2.intProperty, template2.stringProperty, template2.boolProperty);
+        }
+    }
+}
